2cadb4de9a70e27f1e215c67a4c1146bb96306db
Mohammad Amir Ansari <amiransari1721@gmail.com>
2026-01-20 23:32:04 +0530

    start tailwind and used foreach loop and PostController

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,9 +1,25 @@
 <x-app-layout>
-    <x-slot name="header">
+    {{-- <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
             {{ __('Dashboard') }}
         </h2>
-    </x-slot>
+    </x-slot> --}}
+    
+<div class="flex w-4/5 mx-auto justify-center space-x-2 bg-gray-800 p-4 border border-gray-500/50 rounded-md text-md">
+    <div class="flex items-center">
+        <input type="radio" name="options" id="html" class="hidden peer">
+        <label for="html" class="cursor-pointer rounded py-2 px-8 text-white transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">All</label>
+    </div>
+    @foreach ($categories as $category)
+
+    <div class="flex items-center">
+        <input type="radio" name="options" id="html" class="hidden peer">
+        <label for="html" class="cursor-pointer rounded py-2 px-8 text-white transition-colors duration-200 peer-checked:bg-indigo-600 peer-checked:text-white">{{ $category->name }}</label>
+    </div>
+
+    @endforeach
+</div>
+
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
